d5bb7078c59adc0c9de518d4734fec3331f4f6cc
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-12 18:10:24 +0200

    Examples added.

---
+++ examples/basic_op.fm.js
@@ -0,0 +1,1 @@
+var _f = require('formulelib');console.log((new _f.Formule(42)).times(((new _f.Formule(1)).add((new _f.Formule(2)).pow((new _f.Formule(3)))))).minus((new _f.Formule(1))).toString());

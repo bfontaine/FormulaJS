13f2c6b46871d178219b3c54f74d6e012d15ef34
Baptiste Fontaine <batifon@yahoo.fr>
2012-10-17 00:39:25 +0200

    'formula' script can now get a formula in its arguments instead of a file

--- formula-cli.js
+++ formula-cli.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env nodejs
 
-var f = require('./formula'),
-    args = process.argv;
+var f    = require('./formula'),
+    fs   = require('fs'),
+    args = process.argv,
+    source;
 
 args.shift(); // remove 'node'
 
 if (args.length === 1) {
-    console.log("Usage: "+args[0]+" FILE");
+    console.log("Usage:\n\t"+args[0]+" <formula>\n\t"+args[0]+" -f <file>");
+    process.exit(-1);
+}
+
+if (args[1] && fs.existsSync(args[1])) {
+    source = fs.readFileSync(require('path').resolve(args[1]), "utf8");
 }
 else {
-    console.log(f.main(args));
+    source = args.slice(1).join(' ');
 }
+
+console.log(f.parser.parse(source));
